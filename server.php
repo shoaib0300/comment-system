b91bcf16af29f69bfb19be73a46b01e1d2734b69
Shoaib <shoaibsarwar187@gmail.com>
2023-11-17 23:14:49 +0100

    Add Functionality delete comments

--- server.php
+++ server.php
@@ -6,12 +6,24 @@ if (isset($_POST['comment'])) {
     file_put_contents('comments.txt', "$timestamp: $comment" . PHP_EOL, FILE_APPEND);
 }
 
+
+if (isset($_POST['delete'])) {
+    $comments = file('comments.txt');
+    $indexToDelete = $_POST['id'];
+
+    if(isset($comments[$indexToDelete])) {
+        unset($comments[$indexToDelete]);
+        file_put_contents('comments.txt', implode(PHP_EOL, $comments));
+        header('Location: http://localhost/comment-system/');
+    }
+}
+
 $comments = file_get_contents('comments.txt');
 
 // Separate timestamps and comments
 $commentLines = explode(PHP_EOL, $comments);
 
-foreach ($commentLines as $line) {
+foreach ($commentLines as $index => $line) {
     // Split each line into timestamp and comment
     $parts = explode(': ', $line, 2);
     if(empty($line)){
@@ -26,6 +38,13 @@ foreach ($commentLines as $line) {
         echo '<div class="comment">';
         echo '<span class="timestamp">' . $timestamp . '</span>';
         echo '<span class="comment-text">' . $comment . '</span>';
+
+        // add the delete button here
+        echo '<form action="server.php" class="delete-form" name="form" method="POST">';
+        echo '<input type="hidden" name="id" value="' . $index . '">';
+        echo '<button name="delete" value="aA" type="submit" class="delete-button">Delete</button>';
+        echo '</form>';
+
         echo '</div>';
 
     }
